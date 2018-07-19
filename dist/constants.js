9b5627fba2a90e7557ac48e8ae3f4e1bb65de798
Martin Kristof <Martin.Kristof@lmc.eu>
2018-07-19 21:16:17 +0200

    Revert "Temporary commit dist for install as dep directly from github"
    This reverts commit 8b12530334b24f212f98b6fd7d9267b69e02427f.

--- dist/constants.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var ROLLBAR_ENDPOINT = exports.ROLLBAR_ENDPOINT = 'https://api.rollbar.com/api/1/sourcemap';
-
-var ROLLBAR_REQ_FIELDS = exports.ROLLBAR_REQ_FIELDS = ['accessToken', 'version', 'publicPath'];
