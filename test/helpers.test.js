735d5e643b8c9000ccbaf45ef8eefc6a372f1112
Alex Rossi Ignez <rx.rossi@gmail.com>
2019-02-14 11:00:24 +0000

    Adjust test names on helpers test

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -2,15 +2,15 @@ import expect from 'expect';
 import { ROLLBAR_REQ_FIELDS } from '../src/constants';
 import * as helpers from '../src/helpers';
 
-describe('helpers', function() {
-  describe('handleError', function() {
-    it('should return an array of length 1 given a single error', function() {
+describe('helpers', function () {
+  describe('handleError', function () {
+    it('returns an array of length 1 given a single error', function () {
       const result = helpers.handleError(new Error('required field missing'));
       expect(result).toBeA(Array);
       expect(result.length).toEqual(1);
     });
 
-    it('should return an array of length 2 given an array of length 2', function() {
+    it('returns an array of length 2 given an array of length 2', function () {
       const result = helpers.handleError([
         new Error('required field missing'),
         new Error('request failed')
@@ -19,13 +19,13 @@ describe('helpers', function() {
       expect(result.length).toEqual(2);
     });
 
-    it('should prefix message of single error', function() {
+    it('prefixes message of single error', function () {
       const result = helpers.handleError(new Error('required field missing'), 'Plugin');
       expect(result.length).toEqual(1);
       expect(result[0]).toInclude({ message: 'Plugin: required field missing' });
     });
 
-    it('should prefix message of an array of errors', function() {
+    it('prefixes message of an array of errors', function () {
       const result = helpers.handleError([
         new Error('required field missing'),
         new Error('request failed')
@@ -34,7 +34,7 @@ describe('helpers', function() {
       expect(result[0]).toInclude({ message: 'Plugin: required field missing' });
     });
 
-    it('should default prefix to "RollbarSourceMapPlugin"', function() {
+    it('defaults prefix to "RollbarSourceMapPlugin"', function () {
       const result = helpers.handleError(new Error('required field missing'));
       expect(result.length).toEqual(1);
       expect(result[0]).toInclude({
@@ -42,19 +42,19 @@ describe('helpers', function() {
       });
     });
 
-    it('should handle null', function() {
+    it('handles null', function () {
       const result = helpers.handleError(null);
       expect(result).toEqual([]);
     });
 
-    it('should handle empty []', function() {
+    it('handles empty []', function () {
       const result = helpers.handleError([]);
       expect(result).toEqual([]);
     });
   });
 
-  describe('validateOptions', function() {
-    it('should return null if all required options are supplied', function() {
+  describe('validateOptions', function () {
+    it('returns null if all required options are supplied', function () {
       const options = {
         accessToken: 'aaabbbccc000111',
         version: 'latest',
@@ -64,7 +64,7 @@ describe('helpers', function() {
       expect(result).toBe(null); // eslint-disable-line no-unused-expressions
     });
 
-    it('should return an error if accessToken is not supplied', function() {
+    it('returns an error if accessToken is not supplied', function () {
       const options = {
         version: 'latest',
         publicPath: 'https://my.cdn.net/assets'
@@ -76,7 +76,7 @@ describe('helpers', function() {
         .toInclude({ message: 'required field, \'accessToken\', is missing.' });
     });
 
-    it('should return an error if version is not supplied', function() {
+    it('returns an error if version is not supplied', function () {
       const options = {
         accessToken: 'aaabbbccc000111',
         publicPath: 'https://my.cdn.net/assets'
@@ -88,7 +88,7 @@ describe('helpers', function() {
         .toInclude({ message: 'required field, \'version\', is missing.' });
     });
 
-    it('should return an error if publicPath is not supplied', function() {
+    it('returns an error if publicPath is not supplied', function () {
       const options = {
         accessToken: 'aaabbbccc000111',
         version: 'latest'
@@ -100,26 +100,26 @@ describe('helpers', function() {
         .toInclude({ message: 'required field, \'publicPath\', is missing.' });
     });
 
-    it('should handle multiple missing required options', function() {
+    it('handles multiple missing required options', function () {
       const options = {};
       const result = helpers.validateOptions(options);
       expect(result).toBeA(Array);
       expect(result.length).toBe(ROLLBAR_REQ_FIELDS.length);
     });
 
-    it('should handle null for options', function() {
+    it('handles null for options', function () {
       const result = helpers.validateOptions(null);
       expect(result).toBeA(Array);
       expect(result.length).toBe(ROLLBAR_REQ_FIELDS.length);
     });
 
-    it('should handle no options passed', function() {
+    it('handles no options passed', function () {
       const result = helpers.validateOptions();
       expect(result).toBeA(Array);
       expect(result.length).toBe(ROLLBAR_REQ_FIELDS.length);
     });
 
-    it('should return an error if publicPath is not a string nor a function', function () {
+    it('returns an error if publicPath is not a string nor a function', function () {
       const options = {
         accessToken: 'aaabbbccc000111',
         version: 'latest',
@@ -132,11 +132,11 @@ describe('helpers', function() {
         .toInclude({ message: 'invalid type. \'publicPath\' expected to be string or function.' });
     });
 
-    it('should return null if all required arguments are provided and accept a function as the publicPath argument', function () {
+    it('returns null if all required arguments are provided and accept a function as the publicPath argument', function () {
       const options = {
         accessToken: 'aaabbbccc000111',
         version: 'latest',
-        publicPath: () => {}
+        publicPath: () => { }
       };
       const result = helpers.validateOptions(options);
       expect(result).toBe(null);
