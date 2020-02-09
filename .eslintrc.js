691414ae58b41f8d76b4c0be149bd30e825bcbf6
Brandon Doran <bdoran@gmail.com>
2020-02-08 22:54:54 -0800

    Convert eslint config to json

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  env: {
+    jest: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      restParams: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  ignorePatterns: ['node_modules/'],
+  rules: {
+    'comma-dangle': 0,
+    'consistent-return': 0,
+    'func-names': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'prefer-arrow-callback': 0,
+    'space-before-function-paren': 0,
+    'prettier/prettier': [2, { printWidth: 80 }]
+  },
+  globals: {
+    // Constants defined in examples webpack.config
+    __ROLLBAR_ACCESS_TOKEN__: true,
+    __GIT_REVISION__: true
+  }
+};
