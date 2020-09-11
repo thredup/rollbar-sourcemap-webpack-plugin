bceded83e275d0c5dd4a1de5a398e241e6175624
Brandon Doran <bdoran@gmail.com>
2020-09-11 09:56:09 -0700

    chore(examples): add .env support to next-js build (#243)

--- examples/next-js/next.config.js
+++ examples/next-js/next.config.js
@@ -1,17 +1,20 @@
+require('dotenv').config();
+
 const RollbarSourcemapPlugin = require('rollbar-sourcemap-webpack-plugin');
 const withSourceMaps = require('@zeit/next-source-maps')({
   devtool: 'nosources-source-map'
 });
 
 // replace `<ROLLBAR_ACCESS_TOKEN>` with your Rollbar access token
-const ROLLBAR_ACCESS_TOKEN = '<ROLLBAR_ACCESS_TOKEN>';
+const ROLLBAR_ACCESS_TOKEN = process.env.ROLLBAR_SERVER_TOKEN;
 
 module.exports = withSourceMaps({
   env: {
-    ROLLBAR_ACCESS_TOKEN: ROLLBAR_ACCESS_TOKEN
+    ROLLBAR_ACCESS_TOKEN
   },
   webpack: (config, { dev, webpack, buildId }) => {
     if (!dev) {
+      /* eslint-disable-next-line no-param-reassign */
       config.output.futureEmitAssets = false;
       // Generate a common `id` to be used when initializing Rollbar & when uploading the sourcemaps.
       // This could be any common value, as long as it is used in `_document.js` when initializing Rollbar.
